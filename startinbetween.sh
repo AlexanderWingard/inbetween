1dc5f0c18e6f55b0c7d345176de2c571a21fd18c
Fredrik Garneij <fredrik.garneij@gmail.com>
2016-06-04 08:43:16 +0200

    Added monitoring

---
+++ startinbetween.sh
@@ -0,0 +1,2 @@
+DISPLAY=:0 gnome-terminal -e '/home/hdk/processing-3.1.1/processing-java --sketch=/home/hdk/exhibition/inbetween/ --run'
+
---
+++ startreactivision.sh
@@ -0,0 +1,2 @@
+DISPLAY=:0 gnome-terminal -e '/home/hdk/reacTIVision/linux/reacTIVision -c /home/hdk/reacTIVision/linux/reacTIVision.xml'
+
